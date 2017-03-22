f9df5659dcb07d2e2d7be87f3507c022c1eea2ca
Blake Barrett <blake@blakebarrett.net>
2017-03-21 21:27:53 -0700

    Adding different approaches to variable declaration.

--- Swift/Lesson-1/Swift-L1.playground/Contents.swift
+++ Swift/Lesson-1/Swift-L1.playground/Contents.swift
@@ -2,4 +2,11 @@
 
 import UIKit
 
+// As you can already see, Swift makes decalring variables is super-easy!
+
 var str = "Hello, playground"
+
+var hello: String = "Hello!"
+
+var playground: String
+playground = "playground"
